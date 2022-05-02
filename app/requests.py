b48977aac49c4a464110aacd428dd0cc30a7eb93
Clemo97 <lumumbaclement@gmail.com>
2022-05-02 14:40:58 +0300

    get_articles function

--- app/requests.py
+++ app/requests.py
@@ -62,3 +62,19 @@ def process_sources(sources_list):
 
 	return sources_results
 
+def get_articles(id):
+    	'''
+	Function that processes the articles and returns a list of articles objects
+	'''
+	get_articles_url = articles_url.format(id,api_key)
+
+	with urllib.request.urlopen(get_articles_url) as url:
+		articles_results = json.loads(url.read())
+
+
+		articles_object = None
+		if articles_results['articles']:
+			articles_object = process_articles(articles_results['articles'])
+
+	return articles_object
+
