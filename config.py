958c985a20b8c96cfa0253b9aee066ef13539373
Clemo97 <lumumbaclement@gmail.com>
2022-05-03 17:50:51 +0300

    add secret key

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
 	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
 	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
 	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+	SECRET_KEY = os.environ.get('SECRET_KEY')
 	@staticmethod
 	def init_app(app):
 		pass
